e064d3b20d9210f318ec867591098aae97c70cef
Taesun Lee <superbsun@gmail.com>
2020-05-24 17:25:31 +0900

    init

---
+++ src/main/java/com/sunny/ddangnmarket/DdangnMarketApplication.java
@@ -0,0 +1,13 @@
+package com.sunny.ddangnmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdangnMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdangnMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunny/ddangnmarket/DdangnMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunny.ddangnmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdangnMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
