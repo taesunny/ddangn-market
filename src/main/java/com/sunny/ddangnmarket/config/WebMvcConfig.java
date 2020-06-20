172d5edc6bf83f384e8b12b5d4b3b162c90bafc9
Taesun Lee <superbsun@gmail.com>
2020-06-21 00:57:20 +0900

    update WebMvcConfig

--- src/main/java/com/sunny/ddangnmarket/config/WebMvcConfig.java
+++ src/main/java/com/sunny/ddangnmarket/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
