220835ce19aaa91b1e20fd1040c8b2324db2227b
Taesun Lee <superbsun@gmail.com>
2020-07-01 23:00:50 +0900

    add eureka setting

--- src/main/java/com/sunny/ddangnmarket/DdangnMarketApplication.java
+++ src/main/java/com/sunny/ddangnmarket/DdangnMarketApplication.java
@@ -3,8 +3,10 @@ package com.sunny.ddangnmarket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableDiscoveryClient
 @EnableJpaAuditing
 @SpringBootApplication
 public class DdangnMarketApplication {
